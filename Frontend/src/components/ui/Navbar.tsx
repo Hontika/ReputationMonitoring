794f871d6c233405235da0fbdce9eba3e3b44e7d
Karakai Patrik <patrikkarakai@gmail.com>
2024-04-25 13:56:16 +0200

    added logo and things!!

--- Frontend/src/components/ui/Navbar.tsx
+++ Frontend/src/components/ui/Navbar.tsx
@@ -44,11 +44,14 @@ export default function Navbar() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
+                  <NavLink
+                  to={'/'}>
+                    <img
+                      className="h-8 w-auto"
+                      src="/logo.svg"
+                      alt="Your Company"
+                    />
+                  </NavLink>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
