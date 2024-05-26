de74eaa978cccee2b843c8e06541306145df7fbf
Hontika <91741876+Hontika@users.noreply.github.com>
2024-05-26 19:33:47 +0200

    KPI backend part

---
+++ Backend/app/Http/Controllers/UserInteractionController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserInteractionController extends Controller
+{
+    /**
+     * Increment searches count.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function incrementSearches()
+    {
+        $user = Auth::user();
+        $user->increment('searches');
+        return response()->json(['message' => 'Searches count incremented successfully.', 'searches' => $user->searches]);
+    }
+
+    /**
+     * Increment graph interactions count.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function incrementGraphInteractions()
+    {
+        $user = Auth::user();
+        $user->increment('graph_interactions');
+        return response()->json(['message' => 'Graph interactions count incremented successfully.', 'graph_interactions' => $user->graph_interactions]);
+    }
+
+    /**
+     * Increment influencer interactions count.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function incrementInfluencerInteractions()
+    {
+        $user = Auth::user();
+        $user->increment('influencer_interactions');
+        return response()->json(['message' => 'Influencer interactions count incremented successfully.', 'influencer_interactions' => $user->influencer_interactions]);
+    }
+}
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -4,7 +4,12 @@ use App\Http\Controllers\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DataController;
+<<<<<<< Updated upstream
 use App\Http\Controllers\CompanyController;
+=======
+use App\Http\Controllers\UserInteractionController;
+
+>>>>>>> Stashed changes
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +27,9 @@ Route::middleware(['auth:sanctum'])->group(function (){
         return $request->user();
     });
     Route::post('/user/profile', [Profile::class, 'update']);
+    Route::post('/increment-searches', [UserInteractionController::class, 'incrementSearches']);
+    Route::post('/increment-graph-interactions', [UserInteractionController::class, 'incrementGraphInteractions']);
+    Route::post('/increment-influencer-interactions', [UserInteractionController::class, 'incrementInfluencerInteractions']);
 });
 
 Route::get('/fetch-reviews', [DataController::class, 'fetchData']);
